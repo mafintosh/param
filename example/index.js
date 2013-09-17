c3be5bb5f831279f1c4c44949015d27aec4407c9
Mathias Buus <mathiasbuus@gmail.com>
2013-09-17 09:34:40 +0200

    example includes file and env patterns

--- example/index.js
+++ example/index.js
@@ -3,4 +3,5 @@ process.chdir(__dirname);
 var param = require('../index');
 var port = param('app.port');
 
-console.log(port);
+console.log('port is '+port);
+console.log('rest of the config', param());
